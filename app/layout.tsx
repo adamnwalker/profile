c38d4f6278d6e5fb21caf563f569c6189df0c51f
adamnwalker <adam.n.walker@gmail.com>
2026-05-31 17:20:32 +1000

    first pass

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,23 @@
 import type { Metadata } from "next";
+import { Inter } from 'next/font/google'
 import "./globals.css";
 
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
 export const metadata: Metadata = {
-  title: "Adam Taylor",
-  description: "Design engineer based in NSW, Australia.",
+  title: "Adam Walker",
+  description: "Product Designer based in Sydney, Australia.",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body>
         <main>{children}</main>
       </body>
     </html>
-  );
+  )
 }
